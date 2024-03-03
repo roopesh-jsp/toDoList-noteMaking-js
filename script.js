c211d6cf42aea72a111be831fafc362948e32bb3
roopesh-jsp <rupzkumar5@gmail.com>
2024-03-03 15:29:56 +0530

    toDoList done

--- script.js
+++ script.js
@@ -1,2 +1,40 @@
-'using strict';
+"using strict";
 
+// adding task********************
+
+const taskInput = document.querySelector(".input-text");
+const addTask = document.querySelector(".add-btn");
+
+const tasksContainer = document.querySelector(".list");
+let task = ``;
+addTask.addEventListener("click", () => {
+  let x = taskInput.value.replace(/ /g, "");
+  if (x) {
+    task = ` <li class="item">
+    <div class="item-1">
+      <div class="img-status"></div>
+      <p>${taskInput.value}</p>
+    </div>
+    <div class="remove">
+      <button>x</button>
+    </div>
+  </li>`;
+    tasksContainer.insertAdjacentHTML("beforeend", task);
+    taskInput.value = "";
+  }
+});
+
+// task check/uncheck toogle****************
+
+tasksContainer.addEventListener("click", (e) => {
+  let removeBtn = e.target.closest(".remove");
+  if (removeBtn) {
+    tasksContainer.removeChild(removeBtn.closest(".item"));
+  }
+  let item = e.target.closest(".item-1");
+  if (!item) {
+    return;
+  }
+  const imgToggel = item.querySelector(".img-status");
+  imgToggel.classList.toggle("checked");
+});
